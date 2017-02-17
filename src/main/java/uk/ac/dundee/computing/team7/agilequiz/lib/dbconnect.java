2e56154c25353c2388596bb483cdc80784ed53d2
Josh2609 <jcorps@gmx.com>
2017-02-17 21:45:08 +0000

    Added basic login page

---
+++ src/main/java/uk/ac/dundee/computing/team7/agilequiz/lib/dbconnect.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.team7.agilequiz.lib;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Josh Corps
+ * @date 
+ */
+public class dbconnect 
+{
+    public Connection mysqlConnect()
+    {
+	try {  
+	    Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException ex) {
+	    Logger.getLogger(dbconnect.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	Connection con = null;
+	
+	try {
+	    con = DriverManager.getConnection(  
+		    "jdbc:mysql://silva.computing.dundee.ac.uk/:3306/16agileteam7db","16agileteam7","9865.at7.5689"); //db,user,pass
+	    return con;
+	} catch (SQLException ex) {
+	    Logger.getLogger(dbconnect.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	return con;
+    }
+}
---
+++ src/main/java/uk/ac/dundee/computing/team7/agilequiz/servlets/Login.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.team7.agilequiz.servlets;
+
+import java.io.IOException;
+import java.sql.*;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.RequestDispatcher;
+import uk.ac.dundee.computing.team7.agilequiz.lib.dbconnect;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
+import uk.ac.dundee.computing.team7.agilequiz.models.User;
+import uk.ac.dundee.computing.team7.agilequiz.stores.LoggedIn;
+
+/**
+ *
+ * @author Josh Corps
+ */
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException 
+    {
+        // TODO Auto-generated method stub
+    }
+    
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+       RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+       rd.forward(request, response);
+    }
+    
+     /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String matric = request.getParameter("matric");
+        String password = request.getParameter("password");
+	
+	User user = new User();
+	HttpSession session=request.getSession();
+
+	
+	if (user.checkDetails(matric, password))
+	{
+	    LoggedIn lg = new LoggedIn();
+	    lg.setLoggedIn();
+	    lg.setMatric(matric);
+            
+	    session.setAttribute("Matric", matric);
+	    session.setAttribute("LoggedIn", lg);
+	    System.out.println("Session in servlet "+session);
+            RequestDispatcher rd=request.getRequestDispatcher("index.jsp");
+	    rd.forward(request,response);
+            
+	} else {
+	    RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
+	    request.setAttribute("Message", "The Matriculation Number or Password entered is incorrect.");
+	    rd.forward(request, response);
+	}
+    }
+   
+}
